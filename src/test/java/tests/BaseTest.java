b3d20f60ff77a4e04742f5377cb5a53703996e85
Sai Teja <saiteja_saripalli@epam.com>
2022-08-04 14:18:47 +0530

    Plenty

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -2,26 +2,51 @@ package tests;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 import pages.CalculatorPage;
-import java.util.concurrent.TimeUnit;
 public class BaseTest {
     private WebDriver driver;
     protected CalculatorPage calculatorPage;
+
     @BeforeMethod
     public void driverSetup(){
 
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
         driver.manage().window().maximize();
         calculatorPage = new CalculatorPage(driver);
     }
 
-   /* @AfterMethod(alwaysRun = true)
-    public void closeDriver()
-    {
+    @Test
+    public void checkInformationInVmClassString() throws InterruptedException {
+        calculatorPage.openCloudPage();
+        calculatorPage.goToGoogleCloudPlatformPricingCalculatorPage(
+                "Google Cloud Platform Pricing Calculator");
+        calculatorPage.sendKeyInToNumberOfInstancesField("4");
+        calculatorPage.selectOS();
+        calculatorPage.selectProvisionModel();
+        calculatorPage.selectSeriesOfMachine();
+        calculatorPage.selectMachineType();
+        calculatorPage.clickAddGpusCheckBox();
+        calculatorPage.selectGpuType();
+        calculatorPage.selectNumberOfGpus();
+        calculatorPage.selectLocalSsd();
+        calculatorPage.selectDataCenterLocation();
+        calculatorPage.selectCommittedUsage();
+        calculatorPage.pushAddToEstimate();
+        calculatorPage.checkInformationInVmClassIsRegular();
+        calculatorPage.checkInformationInInstanceTypeIncludeN1Standard8();
+        calculatorPage.checkRegionIsFrankfurt();
+        calculatorPage.checkLocalSsdSpace2x375Gib();
+        calculatorPage.checkCommitmentTermOneYear();
+        calculatorPage.checkMonthlyAmount();
+    }
 
+    /*@AfterTest
+    public void closeDriver() {
         driver.close();
     }*/
+
 }
